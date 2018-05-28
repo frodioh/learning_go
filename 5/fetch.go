4fbfdd1c69a0fc0904337ca8f7a495e2a67eed65
Rodion Fedotov <fedotov.dev@gmail.com>
2018-05-29 03:53:10 +0500

    http fetch

--- 5/fetch.go
+++ 5/fetch.go
@@ -1,9 +1,35 @@
+//Fetch выводит ответ на запрос по заданному URL.
 package main
 
 import (
+  "fmt"
+  "io"
+  "net/http"
   "os"
+  "strings"
 )
 
 func main() {
+  for _, url := range os.Args[1:] {
+    if(!(strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://"))) {
+        url = "http://" + url
+        fmt.Println(url)
+    }
 
+    resp, err := http.Get(url)
+
+    if err != nil {
+      fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+      os.Exit(1)
+    }
+
+    io.Copy(os.Stdout, resp.Body)
+    fmt.Println(resp.Status)
+    resp.Body.Close()
+
+    if err != nil {
+      fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
+      os.Exit(1)
+    }
+  }
 }
